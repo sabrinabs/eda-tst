9b50accbec4fa96bb8618a2f58e052b2fea7d92d
Sabrinabs <sabrina.silva@ccc.ufcg.edu.br>
2022-10-08 15:40:23 -0300

    Potencia Recursiva

---
+++ PotenciaRecursiva/PotenciaRecursiva.java
@@ -0,0 +1,23 @@
+package PotenciaRecursiva;
+
+import java.util.Scanner;
+
+class PotenciaRecursiva {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i = Integer.parseInt(scan.nextLine()); 
+        int j = Integer.parseInt(scan.nextLine()); 
+
+        System.out.println(potencia(i,j, 1));
+        scan.close();
+    }
+
+    static int potencia(int i, int j, int out) {
+
+        if(j <= 0){ return out;}
+        out *= i;
+
+        return potencia(i, j-1, out);
+    }
+}
